9cd1a427e58be06869ffd75f1efb52fe17b736bf
aleksi-svaridze <aleksisvaridze@outlook.com>
2024-04-27 22:29:29 +0400

    Add react-icons icons to todo list item

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,18 +1,24 @@
 import { TodosProps } from "../types/types";
-
+import { CiEdit, CiTrash } from "react-icons/ci";
+import { MdOutlineDone } from "react-icons/md";
 
 
  const TodoList: React.FC<TodosProps> = ({todos, setTodos}) => {
   return (
-    <div>
+    <>
        {
             todos.map(todo => (
-              <div key={todo.id} className="bg-blue-300 p-2 rounded-md text-xs cursor-pointer hover:text-orange-500">
-                {todo.todo}
+              <div key={todo.id} className="bg-blue-300 p-2 flex items-center justify-between rounded-md text-xs cursor-pointer">
+                <span>{todo.todo}</span>
+                <div className="flex gap-x-2">
+                    <CiEdit className="w-4 h-4"/>
+                    <CiTrash className="w-4 h-4"/>
+                    <MdOutlineDone className="w-4 h-4"/>
+                </div>
               </div>
             ))
           }
-    </div>
+    </>
   )
 }
 export default TodoList;
